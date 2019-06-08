529885158d933dd3e86f859aa4d5ea49b50b1cca
rabbit-pc <rabbit-pc@DESKTOP-V9MBUMJ>
2019-06-08 17:31:25 +0200

    data model

--- mainBackend/src/main/java/com/example/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.model.Test;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-
-}
---
+++ mainBackend/src/main/java/com/megatravel/run/MainBackendApplication.java
@@ -0,0 +1,22 @@
+package com.megatravel.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+//@EnableAutoConfiguration
+//@ComponentScan(basePackages= {"com.megatravel.*"})
+//@EntityScan("com.megatravel.model")
+//@EnableJpaRepositories("com.megatravel.repository")
+public class MainBackendApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainBackendApplication.class, args);
+	}
+
+}

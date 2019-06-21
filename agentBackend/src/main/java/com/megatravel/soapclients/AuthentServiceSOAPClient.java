259c8005f6c22659a1d1739c92bf6c5ba0b362bc
uroskojo <43353383+uroskojo@users.noreply.github.com>
2019-06-21 09:31:36 +0200

    Delete AuthentServiceSOAPClient.java

--- agentBackend/src/main/java/com/megatravel/soapclients/AuthentServiceSOAPClient.java
+++
@@ -1,8 +0,0 @@
-package com.megatravel.soapclients;
-
-import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
-
-public class AuthentServiceSOAPClient extends WebServiceGatewaySupport{
-
-	//private static final String SERVICE_URI = 
-}
